e56997abce2c2c9a85a0a21ab1c267541ae1b9a1
Ksisioss <54034242+Ksisioss@users.noreply.github.com>
2023-01-26 22:48:50 +0100

    idk

---
+++ src/components/SlotEdit.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div>
+            <div>{{ slotName }}</div>
+            <!-- <span v-for="(item, index) in slot.items" :key="index">{{ item.nom }}, </span> -->
+        </div>
+    </div>
+    
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+
+
+export default {
+    props: ['slotName'],
+    computed: {
+        ...mapGetters(['getCurrentPerso']),
+        slot() {
+            console.log('this.name SlotEdit',this.slotName);
+            console.log('emplqcementsslotedit',this.currentPersos.emplacements.find(element => element.nom === this.name));
+            // console.log(this.getCurrentPerso.emplacements[this.slotConstants.indexOf(this.name)]);
+            return this.getCurrentPerso.emplacements
+        }
+    },
+    created(){
+        console.log("ratooii")
+    }
+
+}
+</script>
+
+<style></style>
